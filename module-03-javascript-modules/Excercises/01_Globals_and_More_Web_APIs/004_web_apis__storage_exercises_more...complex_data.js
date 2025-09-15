184cc9c7958c107e87f5214a42b6b72f945db5df
Renke Brixel <renke.brixel@wbscodingschool.com>
2025-09-15 12:03:17 +0200

    add bundlers and some excercises

---
+++ module-03-javascript-modules/Excercises/01_Globals_and_More_Web_APIs/004_web_apis__storage_exercises_more...complex_data.js
@@ -0,0 +1,77 @@
+// Get elements from DOM
+const form = document.querySelector("form");
+const ul = document.querySelector("ul");
+const reload = document.querySelector("#reload");
+
+const createListItem = (newTask) => {
+  // Addig task to DOM
+  // List item
+  const li = document.createElement("li");
+  li.setAttribute("id", newTask.id);
+  li.textContent = newTask.content;
+  li.classList.add("flex", "items-center", "justify-between", "w-full", "my-2");
+  // Button
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "Delete";
+  deleteButton.classList.add(
+    "px-4",
+    "py-2",
+    "bg-red-500",
+    "hover:bg-red-400",
+    "text-white",
+    "rounded"
+  );
+  // Button event
+  deleteButton.addEventListener("click", () => {
+    const itemToDelete = ul.querySelector(`#${newTask.id}`);
+    // Delete element from storage preserving the rest
+    const existingTasks = JSON.parse(localStorage.getItem("myTasks") || []);
+
+    localStorage.setItem(
+      "myTasks",
+      JSON.stringify(existingTasks.filter((t) => t.id !== newTask.id))
+    );
+
+    itemToDelete.remove();
+  });
+  // Adding button to list item and item to list
+  li.appendChild(deleteButton);
+  return li;
+};
+
+// Add event listener
+form.addEventListener("submit", (e) => {
+  // Prevent default to avoid reload of page
+  e.preventDefault();
+  // Check input has a value
+  const userInput = e.target.elements.userInput.value;
+  if (!userInput) return alert("Please enter something before submitting");
+  // Get previous content from localStorage: key name "myTasks"
+  const myTasks = JSON.parse(localStorage.getItem("myTasks")) || [];
+  // Creating object
+  const newTask = {
+    id: `task-${crypto.randomUUID().replaceAll("-", "")}`,
+    content: userInput,
+  };
+  const li = createListItem(newTask);
+  ul.insertBefore(li, ul.firstElementChild);
+  // Adding task to localStorage
+  myTasks.unshift(newTask);
+  localStorage.setItem("myTasks", JSON.stringify(myTasks));
+  // Reset form
+  e.target.reset();
+});
+// Reload event
+reload.addEventListener("click", () => {
+  window.location.reload();
+});
+
+// Read and load data
+window.addEventListener("load", () => {
+  // Get previous content from localStorage: key name "myTasks"
+  const myTasks = JSON.parse(localStorage.getItem("myTasks")) || [];
+  myTasks.forEach((t) => {
+    const li = createListItem(t);
+    ul.appendChild(li);
+  });
+});
